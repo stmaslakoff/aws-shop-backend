1b5b6a3bf3fd59dc0d2937106e302160c5414900
stmaslakoff <i.maslakoff@softteco.com>
2025-03-01 13:04:13 +0300

    connects DynamoDB to get a products list

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,8 +1,31 @@
-import { products } from '../mocks/data';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+} from "@aws-sdk/lib-dynamodb";
 import { Product } from '../types/product.types';
+import { logger } from '../utils/logger';
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 
 export const getProductsList = async (): Promise<Product[]> => {
-  return products;
+  try {
+    const productsResponse = await docClient.send(new ScanCommand({
+      TableName: process.env.PRODUCTS_TABLE
+    }));
+
+    const products = productsResponse.Items as Product[] || [];
+
+    logger.info('Retrieved products from database', {
+      productsCount: products.length,
+    });
+
+    return products;
+  } catch (error) {
+    logger.error('Error getting products from database', { error });
+    throw error;
+  }
 };
 
 export const getProductById = async (productId: string): Promise<Product | undefined> => {
